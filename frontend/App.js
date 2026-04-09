958e21c67775d5b2d13a6e26f0cc41a96de3fb6b
Suerfine <nathanlgx@gmail.com>
2026-04-10 03:13:33 +0800

    feat: Initial project setup with frontend and backend

---
+++ frontend/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Import Screens
+import AdminCourse from './src/screens/AdminCourse';
+import SideBar from './src/components/SideBar';
+import NavBar from './src/components/NavBar';
+import EditCourseDetails from './src/screens/EditCourseDetail';
+import UserDashboard from './src/screens/UserDashboard';
+
+// Define
+const Stack=createStackNavigator();
+
+export default function App() {
+  const linking = {
+    prefixes: ['http://localhost:8081'], // your dev server URL
+    config: {
+      screens: {
+        'Course Management': 'courseManagement',
+        'Course Details': 'course/:id',
+        'User Dashboard': 'dashboard',
+      },
+    },
+  };
+
+  return (
+    <NavigationContainer
+      linking={linking}
+    >
+
+      <View style={styles.root}>
+        <View style={styles.container}>
+          <SideBar navigation={navigation}/>
+          {/* <NavBar navigation={navigation}/> */}
+          
+          {/* Main Content Area */}
+          <View style={styles.content}>
+            <Stack.Navigator initialRouteName="Course Management">
+              <Stack.Screen name="Course Management" component={AdminCourse} options={{headerShown: false}}/>
+              <Stack.Screen name="Course Details" component={EditCourseDetails}/>
+              <Stack.Screen name="User Dashboard" component={UserDashboard} options={{headerShown: false}}/>
+            </Stack.Navigator>
+          </View>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1, 
+    padding:0,
+    margin:0
+  },
+  container: {
+    flex: 1,           
+    flexDirection: 'row', 
+    // flexDirection: 'column',
+    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    backgroundColor:"#f2f2f2"
+  }
+});
---
+++ frontend/src/components/CourseCard.js
@@ -0,0 +1,87 @@
+import { View, Pressable, Image, Text, StyleSheet} from 'react-native';
+import {BookOpenText, CheckCheck, Timer, ClockAlert} from 'lucide-react-native'
+import ProgressBar from './ProgressBar.js';
+
+const CourseCard=({id,imagePath, courseTitle, numModules,duration,expiry,userType, progress, onPress})=>{
+    return (
+        // Title need change to course ID later
+        <Pressable style={styles.card} onPress={onPress}>
+            <Image source={imagePath} style={styles.courseImg} accessibilityLabel='Cover Photo of Course'/>
+            <Text style={styles.CourseTitle}>{courseTitle}</Text>
+            
+            <View style={styles.courseDetails}>
+                <BookOpenText size={20}/>
+                <Text style={styles.DetailsText}>{numModules} Modules</Text>
+            </View>
+            <View style={styles.courseDetails}>
+                <Timer size={20}/>
+                <Text style={styles.DetailsText}>{duration}</Text>
+            </View>
+            <View style={styles.courseDetails}>
+                <ClockAlert size={20}/>
+                <Text style={styles.DetailsText}>{expiry}</Text>
+            </View>
+            
+            {userType === 'admin' ? (
+                <Pressable style={styles.Publishbtn}>
+                    <CheckCheck/>
+                    <Text>Published</Text>
+                </Pressable>
+            ) : (
+                progress !== undefined && <ProgressBar progress={progress} />
+                
+            )}
+        </Pressable>
+    )
+}
+
+const styles=StyleSheet.create({
+    card:{
+        backgroundColor:"white",
+        borderRadius:5,
+        paddingHorizontal:17,
+        paddingVertical:10,
+        borderRadius:10,
+        maxWidth:300
+    },
+    courseImg:{
+        width:250,
+        height:180,
+        resizeMode:'contain',
+        alignSelf:'center'
+    },
+    CourseTitle:{
+        borderBottomColor:'#8f8f8f',
+        borderBottomWidth:1,
+        fontSize:19,
+        paddingVertical:10,
+        marginBottom:10,
+        textAlign:'center',
+        fontWeight:'bold'
+    },
+    courseDetails:{
+        flexDirection:'row',
+        alignContent:'center',
+        padding:2,
+        color:'#3e3e3e',
+        gap:5,
+        marginBottom:3
+    },
+    DetailsText:{
+        color:'#3e3e3e',
+        fontSize:14
+    },
+    Publishbtn:{
+        flexDirection:'row',
+        gap:8,
+        alignItems:'center',
+        alignSelf:'center',
+        backgroundColor:'#ffb116',
+        borderRadius:10,
+        paddingHorizontal:20,
+        paddingVertical:8,
+        marginTop:15,
+    },
+});
+
+export default CourseCard;
