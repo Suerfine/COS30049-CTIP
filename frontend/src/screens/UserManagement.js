3c274bf6d23d6f8610091040afbe00aa77bcc3be
sinmim-0816 <janefam88@gmail.com>
2026-04-13 11:37:13 +0800

    update usermanagement

---
+++ frontend/src/screens/UserManagement.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import { StyleSheet } from 'react-native';
+
+const UserManagement=()=>{
+    
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default UserManagement;
