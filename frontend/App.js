8c67097a870c1499478f78f1bb2af39d9ef857bc
Aryn <arynjee@gmail.com>
2026-04-16 23:50:26 +0800

    ok switch back to :id instead of :courseId

--- frontend/App.js
+++ frontend/App.js
@@ -26,7 +26,7 @@ export default function App() {
         'Course Details': 'course/:id',
         'User Dashboard': 'dashboard',
         'User Management': 'userManagement',
-        'User Module':'userModule/:courseId',
+        'User Module':'userModule/:id',
         'User Course':'usercourse',
       },
     },
