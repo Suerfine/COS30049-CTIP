1b4c1e1c53ee1ab5a4a456a0fe24d2c0a3da610a
sinmim-0816 <janefam88@gmail.com>
2026-04-26 21:15:53 +0800

    update user profile

--- frontend/src/screens/UserProfile.js
+++ frontend/src/screens/UserProfile.js
@@ -38,10 +38,10 @@ const UserProfile = ({ navigation }) => {
     }, [user, account]);
     
     return(
-        <View style={{ flex: 1 }}>
+        <View style={style.container}>
             <NavBar/>
 
-            <ScrollView style={styles.container}>
+            <ScrollView>
                 {/* Profile Header */}
                 <View style={styles.profileHeader}>
                     {/* Background Image */}
@@ -235,7 +235,7 @@ const UserProfile = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container:{
-        flexDirection: 'column',
+        flex:1,
     },
     // Profile header
     profileHeader:{
