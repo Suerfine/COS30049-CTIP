33969506b5fc8674afa00b79607ef895ed15ec06
sinmim-0816 <janefam88@gmail.com>
2026-05-01 19:59:28 +0800

    update apiconfig and apiendpoints

---
+++ frontend/src/config/ApiEndpoints.js
@@ -0,0 +1,10 @@
+export const API_ENDPOINTS={
+    USER:{
+        SIGNUP: '/registrations',
+        ACCOUNT: '/users',
+        ME:'/users/me',
+    },
+    ADMIN:{
+        APPROVE: (id)=>`registrations/${id}/approve`,
+    }
+};
