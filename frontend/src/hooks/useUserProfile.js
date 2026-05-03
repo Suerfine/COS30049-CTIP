4c6f7c1556c0cd535d78b1ac4df2496c92f64358
Aryn <arynjee@gmail.com>
2026-05-03 23:04:28 +0800

    change pfp modal

--- frontend/src/hooks/useUserProfile.js
+++ frontend/src/hooks/useUserProfile.js
@@ -102,7 +102,7 @@ export const useUserProfile=()=>{
 
     return{
         user,
-        form,
+        form, setForm,
         updateField,
         username,setUsername,
         password,setPassword,
