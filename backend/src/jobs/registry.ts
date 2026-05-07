849911a4c6aee80a17ff1f8dc8bcd6ee526074b3
Suerfine <nathanlgx@gmail.com>
2026-05-07 17:38:00 +0800

    feat(jobs): Add UpdateEnrollmentStatusJob to check for enrollment status updates and send notifications

--- backend/src/jobs/registry.ts
+++ backend/src/jobs/registry.ts
@@ -1,3 +1,4 @@
 // Register and declare all the jobs in this file
 import { CreateEventNotificationJob } from "./handlers/createEventNotificationJob";
-export const jobs = [CreateEventNotificationJob];
+import { UpdateEnrollmentStatusJob } from "./handlers/UpdateEnrollmentStatusJob";
+export const jobs = [CreateEventNotificationJob, UpdateEnrollmentStatusJob];
