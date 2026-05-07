ec839a87366eb59a91d79546a880940a10add3d5
Elleyxx <ellieteng09@gmail.com>
2026-05-07 23:25:28 +0800

    add todo update status

--- frontend/src/screens/UserDashboard.js
+++ frontend/src/screens/UserDashboard.js
@@ -300,16 +300,16 @@ const UserDashboard = ({ navigation }) => {
                                     {filteredEvents.length === 0 ? (
                                         <Text>No todos</Text>
                                     ) : (
-                                        filteredEvents.map(todo => (
-                                            <View key={todo.id} style={styles.todoItem}>
+                                        filteredEvents.map(event => (
+                                            <View key={event.id} style={styles.todoItem}>
                                                 <View style={styles.todoRow}>
                                                     <Checkbox
                                                         value={event.status === 'completed'}
                                                         onValueChange={() => toggleEvent(event.id)}
                                                     />
                                                     <View style={{flex: 1}}>
-                                                        <Text style={styles.todoTitle}>{todo.title}</Text>
-                                                        <Text style={styles.todoCourse}>{todo.course} - {todo.date}</Text>
+                                                        <Text style={styles.todoTitle}>{event.title}</Text>
+                                                        <Text style={styles.todoCourse}>{event.type} - {formatDate(event.event_start_at, false)}</Text>
                                                     </View>
 
                                                     <Pressable onPress={() => openEdit(todo)}>
