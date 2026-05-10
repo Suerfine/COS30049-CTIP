0b6023fc968a871dd601c34252293766960fd6fd
JollyJoy11 <joannecjx.0111@gmail.com>
2026-05-10 19:31:39 +0800

    update course card

--- frontend/src/components/CourseCard.js
+++ frontend/src/components/CourseCard.js
@@ -211,7 +211,6 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: "white",
     borderRadius: 10,
-    maxWidth:'330px',
     elevation: 4,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
