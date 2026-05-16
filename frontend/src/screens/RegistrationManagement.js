23a2faee8df8b424abddcfa9a33717fabb82b32e
sinmim-0816 <janefam88@gmail.com>
2026-05-16 22:49:22 +0800

    fix width for side bar

--- frontend/src/screens/RegistrationManagement.js
+++ frontend/src/screens/RegistrationManagement.js
@@ -793,7 +793,7 @@ const styles = StyleSheet.create({
   sidePanel: {
     width: 350,
     backgroundColor: "white",
-    height: "100%",
+    height: "100vh",
     position: "absolute",
     right: 0,
     top: 0,
