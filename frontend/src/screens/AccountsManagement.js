f2fca892eb206bc51a788d20b46bc9c8a39e4c34
Elleyxx <ellieteng09@gmail.com>
2026-05-18 13:20:44 +0800

    admin enrollment translate

--- frontend/src/screens/AccountsManagement.js
+++ frontend/src/screens/AccountsManagement.js
@@ -565,7 +565,7 @@ const AccountManagement = () => {
               {isEditing ? (
                 <View style={styles.nameEditRow}>
                   <View style={{ flex: 1 }}>
-                    <Text style={styles.panelLabel}>First Name</Text>
+                    <Text style={styles.panelLabel}>{t('first name')}</Text>
                     <TextInput
                       style={[styles.userDetails, styles.inputEditing]}
                       value={editForm?.firstname || ""}
